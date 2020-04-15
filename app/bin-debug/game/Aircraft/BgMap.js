5137757a79ec1d3b3f2c3f9f610dd8226879d615
king <383557980@qq.com>
2020-04-15 22:50:15 +0800

    之前的卡顿是帧数和速度引起的

--- app/bin-debug/game/Aircraft/BgMap.js
+++ app/bin-debug/game/Aircraft/BgMap.js
@@ -15,7 +15,7 @@ var BgMap = (function (_super) {
         // 背景图片宽度
         _this.bgWidth = Utils.getStageWidth();
         // 控制滚动速度
-        _this.bgSpeed = 0.5;
+        _this.bgSpeed = 0.3;
         // 记录上一帧的时间
         _this.timeOnEnterFrame = 0;
         return _this;
@@ -35,16 +35,8 @@ var BgMap = (function (_super) {
             bg.y = -(this.bgHeight * index - this.bgHeight);
             this.addChild(bg);
         }
+        // 开始滚动
         this.startScroll();
-        // var num = 0
-        // setInterval(() => {
-        //     num+=4
-        //     if(num > (this.bgHeight * (this.bgCount-1) - this.bgHeight)){
-        //         num = 0
-        //         this.y = 0
-        //     }
-        //     this.y = num
-        // }, 10)
     };
     // 开始滚动
     BgMap.prototype.startScroll = function () {
@@ -53,15 +45,13 @@ var BgMap = (function (_super) {
     };
     // 滚动方法
     BgMap.prototype.onEnterFrame = function () {
-        // this.y += this.speed
-        /* 获取每一帧的时间差，用时间间隔位置位移会更加平滑 */
+        // 获取每一帧的时间差，用时间间隔位置位移会更加平滑
         var now = egret.getTimer();
         var time = this.timeOnEnterFrame;
         var pass = now - time;
         this.timeOnEnterFrame = now;
         this.y += this.bgSpeed * pass;
-        console.log(this.y);
-        // console.log(object)
+        // 当前视窗容器的y超过所有图片的高度时-1时，恢复默认
         if (this.y > (this.bgHeight * (this.bgCount - 1) - this.bgHeight)) {
             this.y = 0;
         }
--- app/src/game/Aircraft/BgMap.ts
+++ app/src/game/Aircraft/BgMap.ts
@@ -10,11 +10,10 @@ class BgMap extends BaseScene {
     private bgCount: number;
 
     // 控制滚动速度
-    private bgSpeed: number = 0.5;
+    private bgSpeed: number = 0.3;
 
     // 记录上一帧的时间
     private timeOnEnterFrame: number = 0;
-
 
     //  初始化
     initView() {
@@ -28,8 +27,8 @@ class BgMap extends BaseScene {
         this.bgCount = Math.ceil(Utils.getStageHeight() / this.bgHeight) + 2
 
         // 向视图中添加多张背景
-        for (let index = 0; index < this.bgCount; index++) {
 
+        for (let index = 0; index < this.bgCount; index++) {
             let bg = Utils.createBitmapByName('img_bg_level_1_jpg')
             // 让背景宽度充满屏幕
             bg.width = this.bgWidth
@@ -37,17 +36,8 @@ class BgMap extends BaseScene {
             this.addChild(bg)
         }
 
-
+        // 开始滚动
         this.startScroll()
-        // var num = 0
-        // setInterval(() => {
-        //     num+=4
-        //     if(num > (this.bgHeight * (this.bgCount-1) - this.bgHeight)){
-        //         num = 0
-        //         this.y = 0
-        //     }
-        //     this.y = num
-        // }, 10)
     }
 
     // 开始滚动
@@ -58,19 +48,15 @@ class BgMap extends BaseScene {
 
     // 滚动方法
     private onEnterFrame() {
-        // this.y += this.speed
-        /* 获取每一帧的时间差，用时间间隔位置位移会更加平滑 */
+        // 获取每一帧的时间差，用时间间隔位置位移会更加平滑
         const now = egret.getTimer();
         const time = this.timeOnEnterFrame;
         const pass = now - time;
         this.timeOnEnterFrame = now;
 
-
         this.y += this.bgSpeed * pass;
-        console.log(this.y)
 
-        // console.log(object)
-
+        // 当前视窗容器的y超过所有图片的高度时-1时，恢复默认
         if (this.y > (this.bgHeight * (this.bgCount - 1) - this.bgHeight)) {
             this.y = 0
         }
